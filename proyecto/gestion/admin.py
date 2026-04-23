09963cd3e594596c397cba51064396ea886fb029
FelipeUCC898 <felipe.narvaezg@campusucc.edu.co>
2026-04-23 11:30:52 -0500

    creacion de template y model libro

--- proyecto/gestion/admin.py
+++ proyecto/gestion/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'correo', 'nacionalidad', 'fecha_nacimiento')
+
+@admin.register(Libro) 
+class LibroAdmin(admin.ModelAdmin): 
+    list_display = ('titulo', 'genero', 'fecha_publicacion', 'autor', 'isbn') 
+    list_filter = ('autor', 'genero', 'fecha_publicacion') 
